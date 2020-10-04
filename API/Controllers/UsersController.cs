e15b301f5ab2233c7c6d7e65e44701da8c8a9e8c
Ananthakrishnaa1 <ananthakrishnaa1@gmail.com>
2020-10-04 11:06:26 +0530

    api set up

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers {
+    [ApiController] //attributes
+    [Route ("api/[controller]")]
+    public class UsersController : ControllerBase {
+        private readonly DataContext _context;
+        public UsersController (DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        //IEnumerable ===> collections
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers () {
+            return await _context.Users.ToListAsync ();
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser (int id) {
+            return await _context.Users.FindAsync (id);
+        }
+    }
+}
