9140bc4f01a77cb5bd924c748b4ece36147adfab
Ananthakrishnaa1 <ananthakrishnaa1@gmail.com>
2020-10-04 13:02:29 +0530

    view intitalized

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,3 @@
-//configuration being injected into starup class
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,45 +12,38 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-
 namespace API {
     public class Startup {
         private readonly IConfiguration _config;
 
         public Startup (IConfiguration config) {
             _config = config;
-
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
-
-            //lamda expression
             services.AddDbContext<DataContext> (options => {
                 options.UseSqlite (_config.GetConnectionString ("DefaultConnection"));
             });
+            services.AddCors ();
             services.AddControllers ();
 
-            services.AddSwaggerGen (c => {
-                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
         }
-        //dependency injection container .if we want add a class used for entire solution we include her
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
-                app.UseSwagger ();
-                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
-            //middlewares
+
             app.UseHttpsRedirection ();
 
             app.UseRouting ();
 
+            app.UseCors (x => x.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("https://localhost:4200"));
+
             app.UseAuthorization ();
-            //send requests to apis
+
             app.UseEndpoints (endpoints => {
                 endpoints.MapControllers ();
             });
